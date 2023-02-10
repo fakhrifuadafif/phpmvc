ea1c591083bc1f9733c2798b7ab6d314edc1de65
fakhrifuadafif <fakhrifuad57@gmail.com>
2023-02-10 16:13:06 +0700

    2

--- app/core/App.php
+++ app/core/App.php
@@ -8,7 +8,9 @@
         public function pharseURL()
         {
             if ( isset($_GET['url'])){
-                $url = $_GET['url'];
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
                 return $url;
         }
         }
